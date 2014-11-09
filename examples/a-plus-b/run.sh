474fbb4d5465adac48d83890eafaf921ea447bb3
Jun Wu <quark@zju.edu.cn>
2014-11-09 11:29:17 +0800

    Log errors in test script

--- examples/a-plus-b/run.sh
+++ examples/a-plus-b/run.sh
@@ -3,11 +3,32 @@
 LIST="$@"
 [ -z "$LIST" ] && LIST=`ls a* | sed 's#a\.##'`
 
+echo1() {
+  # echo in 1 line
+  echo "$@" | tr -d "\n"
+}
+
+DEBUG_LOG=.debug.$$.log
+ERROR_LOG=error.log
+
 for i in $LIST; do
   src=a.$i
   [ -e $src ] || continue
-  echo '>>' $src ': ' ljudge -u $src -i 1.in -o 1.out -i 2.in -o 2.out --debug
-  ljudge --keep-stdout --keep-stderr --user-code $src --testcase --input 1.in --output 1.out --testcase --input 2.in --output 2.out | cat
-  echo
-  echo
+  echo -n $src': '
+
+  RESULT=`ljudge --debug --keep-stdout --keep-stderr --user-code $src --testcase --input 1.in --output 1.out --testcase --input 2.in --output 2.out 2> $DEBUG_LOG | cat`
+  if [ -z "$RESULT" ] || (echo1 "$RESULT" | grep -qi ERROR) || (echo1 "$RESULT" | grep -qv ACCEPT); then
+    # Log error
+    echo `date` 'Error running' $i 'test' >> $ERROR_LOG
+    echo1 "$RESULT" >> $ERROR_LOG
+    cat $DEBUG_LOG >> $ERROR_LOG
+    echo >> $ERROR_LOG
+    # notify user
+    echo1 'ERROR' "$RESULT"
+    echo 'To re-run: ljudge -u '$src' -i 1.in -o 1.out -i 2.in -o 2.out --debug'
+  else
+    echo OKAY
+  fi
 done 2>/dev/null
+
+[ -e $DEBUG_LOG ] && unlink $DEBUG_LOG
