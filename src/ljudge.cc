09822758e00b5116728359bf098660a56e025b10
Jun Wu <quark@zju.edu.cn>
2014-11-10 13:46:16 +0800

    Use threads only with 2 or more testcases

--- src/ljudge.cc
+++ src/ljudge.cc
@@ -1962,7 +1962,7 @@ static j::value run_testcases(const Options& opts) {
 
   vector<j::value> results;
   results.resize(opts.cases.size());
-  #pragma omp parallel for if (opts.nthread != 1)
+  #pragma omp parallel for if (opts.nthread != 1 && opts.cases.size() > 1)
   for (int i = 0; i < (int)opts.cases.size(); ++i) {
     j::object testcase_result = run_testcase(opts.etc_dir, opts.cache_dir, opts.user_code_path, opts.checker_code_path, opts.envs, opts.cases[i], opts.skip_checker, opts.keep_stdout, opts.keep_stderr);
     results[i] = j::value(testcase_result);
